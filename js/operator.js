9f26cb3fd7055de3d848eeb3be06194bca8e049c
ERLMKea <erlm@kea.dk>
2023-02-17 09:02:42 +0100

    operator.js

---
+++ js/operator.js
@@ -0,0 +1,34 @@
+let a = 1
+console.log(a++)
+console.log(++a)
+let b = 2
+console.log(b**(3+2))
+console.log(b**-3)
+let c = -1
+
+//left to right
+console.log(25-10-5)
+
+//right to left, assignment
+let dyt = 1 + 2 + 3
+
+let x,y
+x = y = 25 - 10 -5
+console.log(x,y)
+
+let minus = -1
+let plus = 5
+console.log(minus)
+console.log(typeof +"b")
+let asci = +"b"
+console.log(asci)
+console.log(typeof 5 + "5")
+console.log(typeof typeof 5)
+if (typeof +"5" == "number" ) console.log('+"5" er et number')
+console.log(("4" + 1) > (4+1))
+console.log(typeof ("4" + 1))
+console.log("4" == 4)
+console.log("4" === 4)
+
+
+
